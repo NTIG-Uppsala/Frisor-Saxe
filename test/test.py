32979d0a0df0fadb1434f3ffb8a4c7204e2a4160
Kastrullen <roder.gumuscu@elev.ga.ntig.se>
2022-09-05 10:25:01 +0200

    Uppdaterade tester.
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: erihedDev <111971782+erihedDev@users.noreply.github.com>
    Co-Authored-By: ahedenskog <111979841+ahedenskog@users.noreply.github.com>

--- test/test.py
+++ test/test.py
@@ -15,14 +15,16 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(service=service, options=options)
 
-def test_find_text_on_page(driver): # Kollar att viktigt innehåll finns på hemsidan
+
+# Kollar att viktigt innehåll finns på hemsidan
+def test_find_text_on_page(driver):
     driver.get("https://ntig-uppsala.github.io/Frisor-Saxe/")
     pageText = driver.find_element(By.TAG_NAME, "body").text
     controlTexts = [
         "Frisör Saxé",
         "Öppettider",
         "Mån-Fre: 10-16",
-        "Lördag: 12-15", 
+        "Lördag: 12-15",
         "Söndag: Stängt!",
         "Adress",
         "Fjällgatan 32H",
@@ -35,14 +37,35 @@ def test_find_text_on_page(driver): # Kollar att viktigt innehåll finns på hem
 
     for text in controlTexts:
         assert text in pageText
+    print("All text content found!")
 
-def test_click_links_on_page(driver): # Klickar på länkarna för att så om dom öppnar en hemsida (Lägg till jämförelse med URL!!!)
+# Klickar på länkarna för att så om dom öppnar en hemsida (Lägg till jämförelse med URL!!!)
+
+
+def test_click_links_on_page(driver):
+    # Facebook
     driver.find_element(By.CSS_SELECTOR, ".fa-facebook").click()
-    assert driver.find_element(By.CSS_SELECTOR, ".fa-facebook").assertTrue()
+    current_title = driver.find_element(
+        By.CSS_SELECTOR, ".fa-facebook").get_attribute("href")
+    assert current_title == "https://facebook.com/ntiuppsala"
+    print("Facebook passed!")
+
+    # Instagram
     driver.find_element(By.CSS_SELECTOR, ".fa-instagram").click()
+    current_title = driver.find_element(
+        By.CSS_SELECTOR, ".fa-instagram").get_attribute("href")
+    assert current_title == "https://instagram.com/ntiuppsala"
+    print("Instagram passed!")
+
+    # Twitter
     driver.find_element(By.CSS_SELECTOR, ".fa-twitter").click()
+    current_title = driver.find_element(
+        By.CSS_SELECTOR, ".fa-twitter").get_attribute("href")
+    assert current_title == "https://twitter.com/ntiuppsala"
+    print("Twitter passed!")
+
 
 test_find_text_on_page(driver)
 test_click_links_on_page(driver)
-    
+
 driver.quit()
