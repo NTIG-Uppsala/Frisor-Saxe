8745aeb0d93042bb13eb7d648ba1f93f741b995f
Linus J <linus.jansson2000@gmail.com>
2022-09-12 10:08:13 +0200

    test refactor
    - added class
    - added loops of duplicate code
    
    Co-Authored-By: erihedDev <111971782+erihedDev@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>

--- test/test.py
+++ test/test.py
@@ -1,67 +1,69 @@
-# OBS!!! Justera inte variabelnamn då det är en del av modulerna, och kan sluta fungera om ändrade
-
-import pytest
+import unittest
+import sys
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service = Service(executable_path=ChromeDriverManager().install())
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=service, options=options)
+class TestClass(unittest.TestCase):
+    website_url = ""
+
+    # Setup selenium options and drivers
+    def setUp(self):
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(service=service, options=options)
 
 
-# Kollar att viktigt innehåll finns på hemsidan
-def test_find_text_on_page(driver):
-    driver.get("https://ntig-uppsala.github.io/Frisor-Saxe/")
-    pageText = driver.find_element(By.TAG_NAME, "body").text
-    controlTexts = [
-        "Frisör Saxé",
-        "Öppettider",
-        "Mån-Fre: 10-16",
-        "Lördag: 12-15",
-        "Söndag: Stängt!",
-        "Adress",
-        "Fjällgatan 32H",
-        "981 39, Kiruna",
-        "Telefonnummer",
-        "0630-555-555",
-        "E-post",
-        "info@ntig-uppsala.github.io"
-    ]
+    # Checks for important info on webpage
+    def test_find_text_on_page(self):
+        self.driver.get(self.website_url)
+        pageText = self.driver.find_element(By.TAG_NAME, "body").text
+        controlTexts = [
+            "Frisör Saxé",
+            "Öppettider",
+            "Mån-Fre: 10-16",
+            "Lördag: 12-15",
+            "Söndag: Stängt!",
+            "Adress",
+            "Fjällgatan 32H",
+            "981 39, Kiruna",
+            "Telefonnummer",
+            "0630-555-555",
+            "E-post",
+            "info@ntig-uppsala.github.io"
+        ]
 
-    for text in controlTexts:
-        assert text in pageText
-    print("All text content found!")
+        for text in controlTexts:
+            self.assertIn(text, pageText)
+            # assert text in pageText
+        print("All text content found!")
 
-# Klickar på länkarna för att så om dom öppnar en hemsida (Lägg till jämförelse med URL!!!)
+    def test_click_links_on_page(self):
+        self.driver.get(self.website_url)
+
+        # List of social medias
+        socials = ['facebook', 'instagram', 'twitter']
+
+        # Loop over list
+        for social in socials:
+            # Check if link and icon is on page
+            icon_element =  self.driver.find_element(By.CLASS_NAME, f"fa-{social}")
+            icon_element.click()
+            icon_href = icon_element.get_attribute("href")
+
+            self.assertEqual(icon_href, f"https://{social}.com/ntiuppsala")
+            print(f"{social} passed!")
 
 
-def test_click_links_on_page(driver):
-    # Facebook
-    driver.find_element(By.CSS_SELECTOR, ".fa-facebook").click()
-    current_title = driver.find_element(By.CSS_SELECTOR, ".fa-facebook").get_attribute("href")
-    assert current_title == "https://facebook.com/ntiuppsala"
-    print("Facebook passed!")
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        TestClass.website_url = sys.argv.pop() # Change url to passed in argument
+    else:
+        TestClass.website_url = "https://ntig-uppsala.github.io/Frisor-Saxe/"
 
-    # Instagram
-    driver.find_element(By.CSS_SELECTOR, ".fa-instagram").click()
-    current_title = driver.find_element(By.CSS_SELECTOR, ".fa-instagram").get_attribute("href")
-    assert current_title == "https://instagram.com/ntiuppsala"
-    print("Instagram passed!")
-
-    # Twitter
-    driver.find_element(By.CSS_SELECTOR, ".fa-twitter").click()
-    current_title = driver.find_element(By.CSS_SELECTOR, ".fa-twitter").get_attribute("href")
-    assert current_title == "https://twitter.com/ntiuppsala"
-    print("Twitter passed!")
-
-test_find_text_on_page(driver)
-test_click_links_on_page(driver)
-
-driver.quit()
+    unittest.main(verbosity=2) # Run unit tests
+    
