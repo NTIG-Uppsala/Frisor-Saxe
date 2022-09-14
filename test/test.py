7cf864d38ec30a15c27d8ce4aac91dc891a2e502
Linus J <linus.jansson2000@gmail.com>
2022-09-14 09:14:00 +0200

    fixed issue which caused screenshots not being saved
    Co-Authored-By: erihedDev <111971782+erihedDev@users.noreply.github.com>
    Co-Authored-By: Kasper-coder <111971931+Kasper-coder@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>

--- test/test.py
+++ test/test.py
@@ -8,8 +8,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
+
 class TestClass(unittest.TestCase):
-    website_image_path = Path(__file__).resolve().parents[1] / Path('root/assets/images/')
+    website_image_path = Path(__file__).resolve(
+    ).parents[1] / Path('root/assets/images/')
     website_url = ""
 
     # Setup selenium options and drivers
@@ -20,8 +22,8 @@ class TestClass(unittest.TestCase):
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(service=service, options=options)
 
-
     # Checks for important info on webpage
+
     def test_find_text_on_page(self):
         self.driver.get(self.website_url)
         page_text = self.driver.find_element(By.TAG_NAME, "body").text
@@ -80,7 +82,8 @@ class TestClass(unittest.TestCase):
         # Loop over list
         for social in socials:
             # Check if link and icon is on page
-            icon_element =  self.driver.find_element(By.CLASS_NAME, f"fa-{social}")
+            icon_element = self.driver.find_element(
+                By.CLASS_NAME, f"fa-{social}")
             print(f"{social} element found")
             ActionChains(icon_element).move_to_element(icon_element).click()
             icon_href = icon_element.get_attribute("href")
@@ -88,31 +91,36 @@ class TestClass(unittest.TestCase):
             self.assertEqual(icon_href, f"https://{social}.com/ntiuppsala")
             print(f"{social} successfully clicked")
 
-
-
     def test_screenshot(self):
         self.driver.get(self.website_url)
-    
+
         resolutions = [
-            [2560, 1440], # 2k desktop
-            [1920, 1080], # desktop
-            [1440, 1080], # laptop
-            [820, 1180], # iPad Air
-            [390, 844], # iPhone 12 Pro
+            [2560, 1440],  # 2k desktop
+            [1920, 1080],  # desktop
+            [1440, 1080],  # laptop
+            [820, 1180],  # iPad Air
+            [390, 844],  # iPhone 12 Pro
         ]
 
+        # Check if screenshots folder exists
+        # example path: 'C:\\Users\\..\\Frisor-Saxe\\test\\test.py\\screenshots'
+        if not Path(Path(__file__).resolve().parent / Path("screenshots")).exists():
+            # if not, create it
+            Path(Path(__file__).resolve().parent / Path("screenshots")).mkdir()
+
         for res in resolutions:
-            x,y = res
+            x, y = res
 
             self.driver.set_window_position(0, 0)
             self.driver.set_window_size(x, y)
-            self.driver.save_screenshot("test/screenshots/screenshot" + str(x) + "x" + str(y) + ".png")
+            self.driver.save_screenshot(
+                "test/screenshots/screenshot" + str(x) + "x" + str(y) + ".png")
             print("saved screenshot with resolution", x, y)
 
             self.driver.set_window_size(y, x)
-            self.driver.save_screenshot("test/screenshots/screenshot" + str(y) + "x" + str(x) + "landscape.png")
+            self.driver.save_screenshot(
+                "test/screenshots/screenshot" + str(y) + "x" + str(x) + "landscape.png")
             print("saved screenshot with resolution", y, x)
-            
 
     def test_for_large_images(self):
         # Assert check for images larger than 1Mb
@@ -122,7 +130,7 @@ class TestClass(unittest.TestCase):
             print("Image path: {} \t image size: {}".format(image, image_size))
             # Assert if the file is greater than 500kb
             self.assertGreater(5e5, image_size)
-    
+
     def test_for_images_on_page(self):
         self.driver.get(self.website_url)
 
@@ -132,27 +140,28 @@ class TestClass(unittest.TestCase):
 
         for image in image_elements:
             _path = ""
-            if image.get_attribute('src') is not None: # if the img has a src attribute with the image
+            # if the img has a src attribute with the image
+            if image.get_attribute('src') is not None:
                 # get src and resolve it as Pathlib Path
                 _path = Path(image.get_attribute('src'))
-            else: # assert False (Just a fail)
+            else:  # assert False (Just a fail)
                 self.assertTrue(False)
                 continue
-            
+
             # append paths filename to paths list
             website_image_paths.append(_path.name)
 
         # assert if all images are present on screen
         for image in self.website_image_path.glob('**/*.jpg'):
-            print("Currently chcking if {} is in {}".format(image.name, website_image_paths))
+            print("Currently chcking if {} is in {}".format(
+                image.name, website_image_paths))
             self.assertIn(image.name, website_image_paths)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        TestClass.website_url = sys.argv.pop() # Change url to passed in argument
+        TestClass.website_url = sys.argv.pop()  # Change url to passed in argument
     else:
         TestClass.website_url = "https://ntig-uppsala.github.io/Frisor-Saxe/"
 
-    unittest.main(verbosity=2) # Run unit tests
-    
+    unittest.main(verbosity=2)  # Run unit tests
