615f325886d5c123b0667d7ece6a4a8c2c72bf0e
Kastrullen <roder.gumuscu@elev.ga.ntig.se>
2022-09-20 14:56:31 +0200

    added test for personnel
    Co-Authored-By: erihedDev <111971782+erihedDev@users.noreply.github.com>
    Co-Authored-By: Linus J <30862662+linus-jansson@users.noreply.github.com>

--- test/test.py
+++ test/test.py
@@ -18,8 +18,9 @@ class TestGlobal(unittest.TestCase):
     """
         This class is used to test features that need to be on all webpages
     """
-    
-    website_image_path = Path(__file__).resolve().parents[1] / Path('root/assets/images/')
+
+    website_image_path = Path(__file__).resolve(
+    ).parents[1] / Path('root/assets/images/')
     website_url = ""
 
     def setUp(self):
@@ -29,28 +30,30 @@ class TestGlobal(unittest.TestCase):
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(service=service, options=options)
 
-        self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
+        # Closes browser instance when tests are done
+        self.addCleanup(self.driver.quit)
 
         self.pages = [
-            'index.html', 
-            # 'personal.html', 
+            'index.html',
+            # 'personal.html',
             'hitta-hit.html'
         ]
-    
+
     def test_check_for_empty_links(self):
-        
+
         for page in self.pages:
             self.driver.get(self.website_url + page)
             print("Testing on page: {}".format(page))
 
             links = self.driver.find_elements(By.TAG_NAME, "a")
-            
+
             for link in links:
-                self.assertNotEqual(link.get_attribute("href").split("/")[-1], "#")
+                self.assertNotEqual(link.get_attribute(
+                    "href").split("/")[-1], "#")
 
     def test_navigation_links(self):
         for page in self.pages:
-            
+
             self.driver.get(self.website_url + page)
             print("Testing on page: {}".format(page))
 
@@ -60,10 +63,11 @@ class TestGlobal(unittest.TestCase):
                 "index.html",
                 "hitta-hit.html"
             ]
-        
+
             # check if all required links are in the navigation
             for link in required_links:
-                self.assertIn(link, [link.get_attribute("href").split('/')[-1] for link in links])
+                self.assertIn(link, [link.get_attribute(
+                    "href").split('/')[-1] for link in links])
 
     def test_for_icons_on_page(self):
         for page in self.pages:
@@ -80,7 +84,8 @@ class TestGlobal(unittest.TestCase):
                 icon_element = self.driver.find_element(
                     By.CLASS_NAME, f"fa-{social}")
                 print(f"{social} element found")
-                ActionChains(icon_element).move_to_element(icon_element).click()
+                ActionChains(icon_element).move_to_element(
+                    icon_element).click()
                 icon_href = icon_element.get_attribute("href")
 
                 self.assertEqual(icon_href, f"https://{social}.com/ntiuppsala")
@@ -97,7 +102,7 @@ class TestGlobal(unittest.TestCase):
 
     def test_for_images_on_page(self):
         for page in self.pages:
-            
+
             self.driver.get(self.website_url + page)
             print("Testing on page: {}".format(page))
 
@@ -124,8 +129,10 @@ class TestGlobal(unittest.TestCase):
                     image.name, website_image_paths))
                 self.assertIn(image.name, website_image_paths)
 
+
 class TestPersonnel(unittest.TestCase):
-    website_image_path = Path(__file__).resolve().parents[1] / Path('root/assets/images/')
+    website_image_path = Path(__file__).resolve(
+    ).parents[1] / Path('root/assets/images/')
     website_url = ""
 
     def setUp(self):
@@ -135,12 +142,36 @@ class TestPersonnel(unittest.TestCase):
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(service=service, options=options)
 
-        self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
+        # Closes browser instance when tests are done
+        self.addCleanup(self.driver.quit)
+
+    def test_find_text_on_page(self):
+        self.driver.get(self.website_url)
+        page_text = self.driver.find_element(By.TAG_NAME, "body").text
+        control_texts = [
+            "Frisör Saxé",
+            "Öppettider",
+            "Kontakt",
+            "0630-555-555",
+            "info@ntig-uppsala.github.io",
+            "Hitta oss",
+            "Fjällgatan 32H",
+            "981 39, Kiruna",
+            "Fredrik barberare",
+            "Öjran barberare",
+            "Anna hair stylist",
+        ]
+
+        for text in control_texts:
+            self.assertIn(text, page_text)
 
     # Check content
     # Check for social links
-class TestFindUs( unittest.TestCase):
-    website_image_path = Path(__file__).resolve().parents[1] / Path('root/assets/images/')
+
+
+class TestFindUs(unittest.TestCase):
+    website_image_path = Path(__file__).resolve(
+    ).parents[1] / Path('root/assets/images/')
     website_url = ""
 
     def setUp(self):
@@ -150,14 +181,15 @@ class TestFindUs( unittest.TestCase):
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(service=service, options=options)
 
-        self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
+        # Closes browser instance when tests are done
+        self.addCleanup(self.driver.quit)
 
     # Check for map
     def test_check_map(self):
         self.driver.get(self.website_url)
         map_url = "google.com/maps/embed?pb=!1m18!1m12!1m3!1d1228.0965925349935!2d20.232261859374567!3d67.86606003621222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45d0ba6368d7c09a3%3A0xe3887ef038c559b0!2sFj%C3%A4llgatan%2032%2C%20981%2039%20Kiruna!5e0!3m2!1sen!2sse!4v1663658499040!5m2!1sen!2sse"
         map_element = self.driver.find_element(By.ID, "map")
-        
+
         self.assertTrue(map_element.is_displayed())
         self.assertIn(map_url, map_element.get_attribute("src"))
 
@@ -182,6 +214,7 @@ class TestFindUs( unittest.TestCase):
 
     # Check for social links
 
+
 class TestIndex(unittest.TestCase):
     website_image_path = Path(__file__).resolve(
     ).parents[1] / Path('root/assets/images/')
@@ -195,7 +228,8 @@ class TestIndex(unittest.TestCase):
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(service=service, options=options)
 
-        self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
+        # Closes browser instance when tests are done
+        self.addCleanup(self.driver.quit)
 
     # Checks for important info on webpage
     def test_find_text_on_page(self):
@@ -217,27 +251,28 @@ class TestIndex(unittest.TestCase):
             self.assertIn(text, page_text)
             # assert text in pageText
 
-    def check_table_content(self, table_id, expected_table_content): 
+    def check_table_content(self, table_id, expected_table_content):
         # loacte table element
-        table_element = self.driver.find_element(By.ID, table_id) 
-        
+        table_element = self.driver.find_element(By.ID, table_id)
+
         # get all tr elements
-        table_row_elements = table_element.find_elements(By.TAG_NAME, "tr") 
-        
+        table_row_elements = table_element.find_elements(By.TAG_NAME, "tr")
+
         # combine tr elements text to one string
-        table_row_text = " ".join([table_row.text for table_row in table_row_elements]) 
+        table_row_text = " ".join(
+            [table_row.text for table_row in table_row_elements])
 
         for product in expected_table_content:
             # combine list of expected row content to one string
             product_joined = " ".join(product)
-            
+
             # assert content on page
             self.assertIn(product_joined, table_row_text)
 
     # Test for open hours
     def test_check_for_open_hours(self):
         self.driver.get(self.website_url)
-        
+
         # List of open hours
         open_hours = [
             ["Mån-Fre", "10 - 16"],
@@ -247,9 +282,10 @@ class TestIndex(unittest.TestCase):
 
         self.check_table_content("openhours", open_hours)
     # test for products on page
+
     def test_check_for_products(self):
         self.driver.get(self.website_url)
-        
+
         # List of products
         products = [
             ["Långt hår", "600 kr"],
@@ -257,15 +293,16 @@ class TestIndex(unittest.TestCase):
             ["Färgning", "560 kr"],
             ["Skägg", "150 kr"],
             ["Toppning", "200 kr"],
-            ["Extensions kort","300 kr"],
-            ["Extensions normal","400 kr"],
-            ["Extensions lång","500 kr"],
-            ["Klippning barn 0-13","150 kr"],
-            ["Långt hår stamkund","300 kr"],
-            ["Kort hår stamkund","250 kr"]
+            ["Extensions kort", "300 kr"],
+            ["Extensions normal", "400 kr"],
+            ["Extensions lång", "500 kr"],
+            ["Klippning barn 0-13", "150 kr"],
+            ["Långt hår stamkund", "300 kr"],
+            ["Kort hår stamkund", "250 kr"]
         ]
 
         self.check_table_content("products", products)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
