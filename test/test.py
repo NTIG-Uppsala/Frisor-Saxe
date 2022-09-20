15832f3389b11f4b78345a9d86389e1325da437a
Linus J <linus.jansson2000@gmail.com>
2022-09-20 10:28:10 +0200

    test for different pages
    Co-Authored-By: erihedDev <111971782+erihedDev@users.noreply.github.com>
    Co-Authored-By: RoderXD <111990906+RoderXD@users.noreply.github.com>

--- test/test.py
+++ test/test.py
@@ -14,7 +14,170 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class TestClass(unittest.TestCase):
+class TestGlobal(unittest.TestCase):
+    """
+        This class is used to test stuff that need to be on all webpages
+    """
+    
+    website_image_path = Path(__file__).resolve().parents[1] / Path('root/assets/images/')
+    website_url = ""
+
+    def setUp(self):
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(service=service, options=options)
+
+        self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
+
+        self.pages = [
+            'index.html', 
+            'personal.html', 
+            'hitta-hit.html'
+        ]
+    
+    def test_check_for_empty_links(self):
+        self.driver.get(self.website_url)
+        links = self.driver.find_elements(By.TAG_NAME, "a")
+
+        for link in links:
+            self.assertNotEqual(link.get_attribute("href").split("/")[-1], "#")
+
+    def test_navigation_links(self):
+        for page in self.pages:
+            
+            self.driver.get(self.website_url + page)
+
+            navigation = self.driver.find_element(By.TAG_NAME, "nav")
+            links = navigation.find_elements(By.TAG_NAME, "a")
+            required_links = [
+                "#header",
+                "#openhours",
+                "#products",
+                "#contact"
+            ]
+        
+            # check if all required links are in the navigation
+            for link in required_links:
+                self.assertIn(link, [link.get_attribute("href").split('/')[-1] for link in links])
+
+    def test_for_icons_on_page(self):
+        for page in self.pages:
+
+            self.driver.get(self.website_url + page)
+
+            # List of social medias
+            socials = ['facebook', 'instagram', 'twitter']
+
+            # Loop over list
+            for social in socials:
+                # Check if link and icon is on page
+                icon_element = self.driver.find_element(
+                    By.CLASS_NAME, f"fa-{social}")
+                print(f"{social} element found")
+                ActionChains(icon_element).move_to_element(icon_element).click()
+                icon_href = icon_element.get_attribute("href")
+
+                self.assertEqual(icon_href, f"https://{social}.com/ntiuppsala")
+                print(f"{social} successfully clicked")
+
+    def test_for_large_images(self):
+        # Assert check for images larger than 1Mb
+        for image in self.website_image_path.glob('**/*.*'):
+            # Get the file size property
+            image_size = Path(image).stat().st_size
+            print("Image path: {} \t image size: {}".format(image, image_size))
+            # Assert if the file is greater than 500kb
+            self.assertGreater(500_000, image_size)
+
+    def test_for_images_on_page(self):
+        for page in self.pages:
+            
+            self.driver.get(self.website_url + page)
+
+            # get all elements with img tag
+            image_elements = self.driver.find_elements(By.TAG_NAME, 'img')
+            website_image_paths = []
+
+            for image in image_elements:
+                _path = ""
+                # if the img has a src attribute with the image
+                if image.get_attribute('src') is not None:
+                    # get src and resolve it as Pathlib Path
+                    _path = Path(image.get_attribute('src'))
+                else:  # assert False (Just a fail)
+                    self.assertTrue(False)
+                    continue
+
+                # append paths filename to paths list
+                website_image_paths.append(_path.name)
+
+            # assert if all images are present on screen
+            for image in self.website_image_path.glob('**/*.jpg'):
+                print("Currently chcking if {} is in {}".format(
+                    image.name, website_image_paths))
+                self.assertIn(image.name, website_image_paths)
+
+class TestPersonnel(unittest.TestCase):
+    website_image_path = Path(__file__).resolve().parents[1] / Path('root/assets/images/')
+    website_url = ""
+
+    def setUp(self):
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(service=service, options=options)
+
+        self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
+
+    # Check content
+    # Check for social links
+class TestFindUs( unittest.TestCase):
+    website_image_path = Path(__file__).resolve().parents[1] / Path('root/assets/images/')
+    website_url = ""
+
+    def setUp(self):
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(service=service, options=options)
+
+        self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
+
+    # Check for map
+    def test_check_map(self):
+        self.driver.get(self.website_url)
+        map_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1228.0965925349935!2d20.232261859374567!3d67.86606003621222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45d0ba6368d7c9a3%3A0xe3887ef038c559b0!2sFj%C3%A4llgatan%2032%2C%20981%2039%20Kiruna!5e0!3m2!1sen!2sse!4v1663658499040!5m2!1sen!2sse"
+        map_element = self.driver.find_element(By.ID, "map")
+        
+        self.assertTrue(map_element.is_displayed())
+        self.assertEqual(map_url, map_element.get_attribute("src"))
+
+    # Check content
+    def test_find_text_on_page(self):
+        self.driver.get(self.website_url)
+        page_text = self.driver.find_element(By.TAG_NAME, "body").text
+        control_texts = [
+            "Frisör Saxé",
+            "Öppettider",
+            "Kontakt",
+            "0630-555-555",
+            "info@ntig-uppsala.github.io",
+            "Hitta oss",
+            "Fjällgatan 32H",
+            "981 39, Kiruna"
+        ]
+
+        for text in control_texts:
+            self.assertIn(text, page_text)
+            # assert text in pageText
+
+    # Check for social links
+
+class TestIndex(unittest.TestCase):
     website_image_path = Path(__file__).resolve(
     ).parents[1] / Path('root/assets/images/')
     website_url = ""
@@ -30,7 +193,6 @@ class TestClass(unittest.TestCase):
         self.addCleanup(self.driver.quit) # Closes browser instance when tests are done
 
     # Checks for important info on webpage
-
     def test_find_text_on_page(self):
         self.driver.get(self.website_url)
         page_text = self.driver.find_element(By.TAG_NAME, "body").text
@@ -49,7 +211,6 @@ class TestClass(unittest.TestCase):
         for text in control_texts:
             self.assertIn(text, page_text)
             # assert text in pageText
-        print("All text content found!")
 
     def check_table_content(self, table_id, expected_table_content): 
         # loacte table element
@@ -68,6 +229,18 @@ class TestClass(unittest.TestCase):
             # assert content on page
             self.assertIn(product_joined, table_row_text)
 
+    # Test for open hours
+    def test_check_for_open_hours(self):
+        self.driver.get(self.website_url)
+        
+        # List of open hours
+        open_hours = [
+            ["Mån-Fre", "10 - 16"],
+            ["Lördag", "12 - 15"],
+            ["Söndag", "Stängt"]
+        ]
+
+        self.check_table_content("openhours", open_hours)
     # test for products on page
     def test_check_for_products(self):
         self.driver.get(self.website_url)
@@ -88,154 +261,78 @@ class TestClass(unittest.TestCase):
         ]
 
         self.check_table_content("products", products)
-                
-    # Test for open hours
-    def test_check_for_open_hours(self):
-        self.driver.get(self.website_url)
-        
-        # List of open hours
-        open_hours = [
-            ["Mån-Fre", "10 - 16"],
-            ["Lördag", "12 - 15"],
-            ["Söndag", "Stängt"]
-        ]
 
-        self.check_table_content("openhours", open_hours)
-
-    def test_check_for_empty_links(self):
-        self.driver.get(self.website_url)
-        links = self.driver.find_elements(By.TAG_NAME, "a")
-
-        for link in links:
-            self.assertNotEqual(link.get_attribute("href").split("/")[-1], "#")
-
-    def test_navigation_links(self):
-        self.driver.get(self.website_url)
-
-        navigation = self.driver.find_element(By.TAG_NAME, "nav")
-        links = navigation.find_elements(By.TAG_NAME, "a")
-        required_links = [
-            "#header",
-            "#openhours",
-            "#products",
-            "#contact"
-        ]
     
-        # check if all required links are in the navigation
-        for link in required_links:
-            self.assertIn(link, [link.get_attribute("href").split('/')[-1] for link in links])
+    def test_screenshot(self):
+        # reference: https://www.browserstack.com/guide/ideal-screen-sizes-for-responsive-design
+        resolutions = [
+            [1920, 1080 , "1920x1080"],
+            [2560, 1440, "2560x1440"],
+            [1366, 768, "1366x768"],
+            [360, 640 , "360x640"],  
+            [820, 1180, "820x1180"], 
+            [414, 896, "414x896"], 
+            [1536, 864, "1536x864"],  
+        ]
 
-    def test_for_icons_on_page(self):
-        self.driver.get(self.website_url)
+        page_sections = [
+            "header",
+            "openhours",
+            "products",
+            "contact"
+        ]
 
-        # List of social medias
-        socials = ['facebook', 'instagram', 'twitter']
+        for res in resolutions:
+            x, y, device_name = res
 
-        # Loop over list
-        for social in socials:
-            # Check if link and icon is on page
-            icon_element = self.driver.find_element(
-                By.CLASS_NAME, f"fa-{social}")
-            print(f"{social} element found")
-            ActionChains(icon_element).move_to_element(icon_element).click()
-            icon_href = icon_element.get_attribute("href")
-
-            self.assertEqual(icon_href, f"https://{social}.com/ntiuppsala")
-            print(f"{social} successfully clicked")
-
-    # def test_screenshot(self):
-    #     # reference: https://www.browserstack.com/guide/ideal-screen-sizes-for-responsive-design
-    #     resolutions = [
-    #         [1920, 1080 , "1920x1080"],
-    #         [2560, 1440, "2560x1440"],
-    #         [1366, 768, "1366x768"],
-    #         [360, 640 , "360x640"],  
-    #         [820, 1180, "820x1180"], 
-    #         [414, 896, "414x896"], 
-    #         [1536, 864, "1536x864"],  
-    #     ]
-
-    #     page_sections = [
-    #         "header",
-    #         "openhours",
-    #         "products",
-    #         "contact"
-    #     ]
-    #     for res in resolutions:
-    #         x, y, device_name = res
-
-    #         self.driver.set_window_position(0, 0)
-    #         self.driver.set_window_size(width=x, height=y)
+            self.driver.set_window_position(0, 0)
+            self.driver.set_window_size(width=x, height=y)
             
-    #         for section in page_sections:
-    #             image_path = Path(__file__).resolve().parent / Path(f'screenshots/{device_name}')
-    #             screenshot_path = str(image_path) + f"/{section}_{device_name}.png"
+            for section in page_sections:
+                image_path = Path(__file__).resolve().parent / Path(f'screenshots/{device_name}')
+                screenshot_path = str(image_path) + f"/{section}_{device_name}.png"
 
-    #             # Check if screenshots folder exists
-    #             # example path: 'C:\\Users\\..\\Frisor-Saxe\\test\\test.py\\screenshots\\device_name'
-    #             if not Path(image_path).exists():
-    #                 # if not, create it
-    #                 Path(image_path).mkdir(parents=True)
+                # Check if screenshots folder exists
+                # example path: 'C:\\Users\\..\\Frisor-Saxe\\test\\test.py\\screenshots\\device_name'
+                if not Path(image_path).exists():
+                    # if not, create it
+                    Path(image_path).mkdir(parents=True)
 
-    #             self.driver.get(self.website_url + f"#{section}") # open the page
-    #             time.sleep(5) # sleep for 5 seconds to let the page load
+                self.driver.get(self.website_url + f"#{section}") # open the page
+                time.sleep(5) # sleep for 5 seconds to let the page load
                 
-    #             try:
-    #                 current_section_element = self.driver.find_element(By.ID, section) # find the section
-    #                 ActionChains(current_section_element).move_to_element(current_section_element) # Move to the section
-    #                 WebDriverWait(self.driver, 20).until(EC.visibility_of((current_section_element))) # Wait for the section to be visible
-    #                 current_section_element.screenshot(screenshot_path) # Take screenshot of section
-    #                 print("saved screenshot with", device_name, "at", section)
-    #             except Exception as err:
-    #                 # Catch any errors and print them and continue
-    #                 print(err)
-    #                 print("Could not save screenshot of", section, "with", device_name)
+                try:
+                    current_section_element = self.driver.find_element(By.ID, section) # find the section
+                    ActionChains(current_section_element).move_to_element(current_section_element) # Move to the section
+                    WebDriverWait(self.driver, 20).until(EC.visibility_of((current_section_element))) # Wait for the section to be visible
+                    current_section_element.screenshot(screenshot_path) # Take screenshot of section
+                    print("saved screenshot with", device_name, "at", section)
+                except Exception as err:
+                    # Catch any errors and print them and continue
+                    print(err)
+                    print("Could not save screenshot of", section, "with", device_name)
         
-    #     # assert the correct amount of images
-    #     expected_images = len(resolutions) * len(page_sections)
-    #     actual_images = len(list(Path(__file__).resolve().parent.glob('screenshots/**/*.png')))
-    #     self.assertEqual(expected_images, actual_images)
+        # assert the correct amount of images
+        expected_images = len(resolutions) * len(page_sections)
+        actual_images = len(list(Path(__file__).resolve().parent.glob('screenshots/**/*.png')))
+        self.assertEqual(expected_images, actual_images)
 
-    def test_for_large_images(self):
-        # Assert check for images larger than 1Mb
-        for image in self.website_image_path.glob('**/*.*'):
-            # Get the file size property
-            image_size = Path(image).stat().st_size
-            print("Image path: {} \t image size: {}".format(image, image_size))
-            # Assert if the file is greater than 500kb
-            self.assertGreater(500_000, image_size)
-
-    def test_for_images_on_page(self):
-        self.driver.get(self.website_url)
-
-        # get all elements with img tag
-        image_elements = self.driver.find_elements(By.TAG_NAME, 'img')
-        website_image_paths = []
-
-        for image in image_elements:
-            _path = ""
-            # if the img has a src attribute with the image
-            if image.get_attribute('src') is not None:
-                # get src and resolve it as Pathlib Path
-                _path = Path(image.get_attribute('src'))
-            else:  # assert False (Just a fail)
-                self.assertTrue(False)
-                continue
-
-            # append paths filename to paths list
-            website_image_paths.append(_path.name)
-
-        # assert if all images are present on screen
-        for image in self.website_image_path.glob('**/*.jpg'):
-            print("Currently chcking if {} is in {}".format(
-                image.name, website_image_paths))
-            self.assertIn(image.name, website_image_paths)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        TestClass.website_url = sys.argv.pop()  # Change url to passed in argument
+        arg = sys.argv.pop()
+        # Change url to passed in argument
+        TestGlobal.website_url = arg
+        TestIndex.website_url = arg + "index.html"
+        TestPersonnel.website_url = arg + "personal.html"
+        TestFindUs.website_url = arg + "hitta-hit.html"
+
     else:
-        TestClass.website_url = "https://ntig-uppsala.github.io/Frisor-Saxe/"
+        # if no argument is passed in, test on live website
+        TestGlobal.website_url = "https://ntig-uppsala.github.io/Frisor-Saxe/"
+        TestIndex.website_url = "https://ntig-uppsala.github.io/Frisor-Saxe/index.html"
+        TestPersonnel.website_url = "https://ntig-uppsala.github.io/Frisor-Saxe/personal.html"
+        TestFindUs.website_url = "https://ntig-uppsala.github.io/Frisor-Saxe/hitta-hit.html"
 
     unittest.main(verbosity=2)  # Run unit tests
